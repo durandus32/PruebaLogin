0b14e9c662d3cd56e74710e17964b86d74ad4310
Christian D <durandus32@gmail.com>
2017-02-21 17:37:55 -0600

    Archivo de configuracion de base de datos

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '6QWCm9Drmdy96fVI');
+   define('DB_DATABASE', 'PruebaLogin');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   //dasdasldmlasm
+?>
